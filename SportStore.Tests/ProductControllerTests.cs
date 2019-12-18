84bb0ddc77089a9d3e62200c34b80dc34ce76ec5
Domingo Javier Martinez Torrivilla <Domingo_Martinez@Dell.com>
2019-12-18 10:18:11 -0600

    Addind cart basic support

--- SportStore.Tests/ProductControllerTests.cs
+++ SportStore.Tests/ProductControllerTests.cs
@@ -1,6 +1,9 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using SportsStore.Controllers;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -9,6 +12,34 @@ namespace SportsStore.Tests
 {
     public class ProductControllerTests
     {
+
+        [Fact]
+        public void Can_Send_pagination_View_Model()
+        {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product{ProductId=1, Name= "P1"},
+                new Product{ProductId=2, Name= "P2"},
+                new Product{ProductId=3, Name= "P3"},
+                new Product{ProductId=4, Name= "P4"},
+                new Product{ProductId=5, Name= "P5"}
+            }).AsQueryable<Product>());
+
+            ProductController controller = new ProductController(mock.Object) { PageSize = 3 };
+            
+            // Act
+            ProductsListViewModel result = controller.List(null, 2).ViewData.Model as ProductsListViewModel;
+
+            // Assert
+            PagingInfo pagingInfo = result.PagingInfo;
+            Assert.Equal(2, pagingInfo.CurrentPage);
+            Assert.Equal(3, pagingInfo.ItemsPerPage);
+            Assert.Equal(5, pagingInfo.TotalItems);
+            Assert.Equal(2, pagingInfo.TotalPages);
+        }
+
         [Fact]
         public void Can_Paginate()
         {
@@ -27,14 +58,72 @@ namespace SportsStore.Tests
             controller.PageSize = 3;
 
             // Act
-            IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+            ProductsListViewModel result = controller.List(null, 2).ViewData.Model as ProductsListViewModel;
 
             // Assert
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.True(prodArray.Length == 2);
             Assert.Equal("P4", prodArray[0].Name);
             Assert.Equal("P5", prodArray[1].Name);
         }
 
+        [Fact]
+        public void Can_Filter_products()
+        {
+            // Arrange
+            // - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product{ProductId=1, Name= "P1", Category = "Cat1"},
+                new Product{ProductId=2, Name= "P2", Category = "Cat2"},
+                new Product{ProductId=3, Name= "P3", Category = "Cat1"},
+                new Product{ProductId=4, Name= "P4", Category = "Cat2"},
+                new Product{ProductId=5, Name= "P5", Category = "Cat3"}
+            }).AsQueryable<Product>());
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // Action
+            Product[] result = (controller.List("Cat2").ViewData.Model as ProductsListViewModel).Products.ToArray();
+
+            // Assert
+            Assert.Equal(2, result.Length);
+            Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.True(result[1].Name == "P4" && result[0].Category == "Cat2");
+        }
+
+        [Fact]
+        public void Generate_Category_Specific_product_count()
+        {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product{ProductId=1, Name= "P1", Category = "Cat1"},
+                new Product{ProductId=2, Name= "P2", Category = "Cat2"},
+                new Product{ProductId=3, Name= "P3", Category = "Cat1"},
+                new Product{ProductId=4, Name= "P4", Category = "Cat2"},
+                new Product{ProductId=5, Name= "P5", Category = "Cat3"}
+            }).AsQueryable<Product>());
+
+            ProductController target = new ProductController(mock.Object);
+            target.PageSize = 3;
+
+            Func<ViewResult, ProductsListViewModel> GetModel = result => result?.ViewData?.Model as ProductsListViewModel;
+
+            // Action
+            int? rest1 = GetModel(target.List("Cat1"))?.PagingInfo.TotalItems;
+            int? rest2 = GetModel(target.List("Cat2"))?.PagingInfo.TotalItems;
+            int? rest3 = GetModel(target.List("Cat3"))?.PagingInfo.TotalItems;
+            int? restAll = GetModel(target.List(null))?.PagingInfo.TotalItems;
+
+            // Assert
+            Assert.Equal(2, rest1);
+            Assert.Equal(2, rest2);
+            Assert.Equal(1, rest3);
+            Assert.Equal(5, restAll);
+        }
     }
 }
--- SportStore/Startup.cs
+++ SportStore/Startup.cs
@@ -22,6 +22,8 @@ namespace SportStore
                 Configuration["Data:SportStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +32,29 @@ namespace SportStore
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseMvc(routes => {
                 routes.MapRoute(
-                    name: "defaul",
-                    template: "{controller=Product}/{action=List}/{id?}");
+                    name: null,
+                    template: "{category}/Page{productPage:int}",
+                    defaults: new { controller = "Product", action = "List" });
+
+                routes.MapRoute(
+                    name: null,
+                    template: "Page{productPage:int}",
+                    defaults: new { Controller = "Product", action = "List", productPage = 1 });
+
+                routes.MapRoute(
+                    name: null,
+                    template: "{category}",
+                    defaults: new { Controller = "Product", action = "List", productPage = 1 });
+
+                routes.MapRoute(
+                    name: null,
+                    template: "",
+                    defaults: new { Controller = "Product", action = "List", productPage = 1 });
+
+                routes.MapRoute( name: null, template: "{controller}/{action}/{id?}");
             });
             SeedData.EnsurePopulated(app);
         }
